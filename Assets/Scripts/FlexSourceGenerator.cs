ab4027b38a73c59b335f439dec402a566874ec68
Kazuaki Hyoda <kazuaki@ex-t.jp>
2018-10-15 13:53:21 +0900

    [Add]水源オブジェクトを自動で大量に設置するスクリプトを追加 暫定として横一列のみ

--- Assets/Scripts/FlexSourceGenerator.cs
+++ Assets/Scripts/FlexSourceGenerator.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class FlexSourceGenerator : MonoBehaviour {
 
+    public GameObject prefabFlexSourceActor;
+    public float distance = 0.5f;
+    public Vector3 startPosition = new Vector3(-12.0f, 9.45f, 2.06f);
+    public RandomRange randomRange = new RandomRange(-1.0f, 1.0f);
+    public int count = 50;
+
 	// Use this for initialization
 	void Start () {
-		
+		for(int i = 0; i < count; i++)
+        {
+            GameObject obj = Instantiate(prefabFlexSourceActor, new Vector3(startPosition.x + distance * i, startPosition.y + Random.Range(randomRange.Min, randomRange.Max), startPosition.z), Quaternion.identity);
+            obj.transform.parent = transform;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+}
+
+public class RandomRange {
+    private float _min;
+    private float _max;
+
+    public RandomRange(float min, float max)
+    {
+        _min = (min < max) ? min : max;
+        _max = (min < max) ? max : min;
+    }
+
+    public float Min
+    {
+        set { _min = (value < _max) ? value : _min; }
+        get { return _min; }
+    }
+
+    public float Max
+    {
+        set { _max = (value > _min) ? value : _max;  }
+        get { return _max; }
+    }
 }
