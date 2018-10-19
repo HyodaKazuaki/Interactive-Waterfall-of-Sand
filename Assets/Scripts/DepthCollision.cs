83b9d00099c327b5420cf2bda6427e947686f144
Kazuaki Hyoda <kazuaki@ex-t.jp>
2018-10-19 17:48:07 +0900

    [Fix]様々な修正
    詳しくはRelaseに書く予定

--- Assets/Scripts/DepthCollision.cs
+++ Assets/Scripts/DepthCollision.cs
@@ -3,108 +3,118 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DepthCollision : MonoBehaviour {
+public class DepthCollision : MonoBehaviour
+{
 
-    // background image display instance
-    public Image backgroundImage;
+	// background image display instance
+	public Image backgroundImage;
 
-    // the KinectManager instance
-    private KinectManager manager;
+	//Physics Material
+	public PhysicMaterial physicMaterial;
 
-    // the foreground texture
-    //private Texture2D foregroundTex;
+	// the KinectManager instance
+	private KinectManager manager;
 
-    // rectangle taken by the foreground texture (in pixels)
-    private Rect foregroundRect;
-    private Vector2 foregroundOfs;
+	// the foreground texture
+	//private Texture2D foregroundTex;
 
-    // game objects to contain the joint colliders
-    private GameObject[] jointColliders = null;
+	// rectangle taken by the foreground texture (in pixels)
+	private Rect foregroundRect;
+	private Vector2 foregroundOfs;
 
-    // Use this for initialization
-    void Start () {
-        // calculate the foreground rectangle
-        Rect cameraRect = Camera.main.pixelRect;
-        float rectHeight = cameraRect.height;
-        float rectWidth = cameraRect.width;
+	// game objects to contain the joint colliders
+	private GameObject[] jointColliders = null;
 
-        if (rectWidth > rectHeight)
-            rectWidth = rectHeight * KinectWrapper.Constants.DepthImageWidth / KinectWrapper.Constants.DepthImageHeight;
-        else
-            rectHeight = rectWidth * KinectWrapper.Constants.DepthImageHeight / KinectWrapper.Constants.DepthImageWidth;
+	// Use this for initialization
+	void Start()
+	{
+		// calculate the foreground rectangle
+		Rect cameraRect = Camera.main.pixelRect;
+		float rectHeight = cameraRect.height;
+		float rectWidth = cameraRect.width;
 
-        foregroundOfs = new Vector2((cameraRect.width - rectWidth) / 2, (cameraRect.height - rectHeight) / 2);
-        foregroundRect = new Rect(foregroundOfs.x, cameraRect.height - foregroundOfs.y, rectWidth, -rectHeight);
+		if (rectWidth > rectHeight)
+			rectWidth = rectHeight * KinectWrapper.Constants.DepthImageWidth / KinectWrapper.Constants.DepthImageHeight;
+		else
+			rectHeight = rectWidth * KinectWrapper.Constants.DepthImageHeight / KinectWrapper.Constants.DepthImageWidth;
 
-        // create joint colliders
-        int numColliders = (int)KinectWrapper.NuiSkeletonPositionIndex.Count;
-        jointColliders = new GameObject[numColliders];
+		foregroundOfs = new Vector2((cameraRect.width - rectWidth) / 2, (cameraRect.height - rectHeight) / 2);
+		foregroundRect = new Rect(foregroundOfs.x, cameraRect.height - foregroundOfs.y, rectWidth, -rectHeight);
 
-        for (int i = 0; i < numColliders; i++)
-        {
-            string sColObjectName = ((KinectWrapper.NuiSkeletonPositionIndex)i).ToString() + "Collider";
-            jointColliders[i] = new GameObject(sColObjectName);
-            jointColliders[i].transform.parent = transform;
+		// create joint colliders
+		int numColliders = (int)KinectWrapper.NuiSkeletonPositionIndex.Count;
+		jointColliders = new GameObject[numColliders * 2];
 
-            CapsuleCollider collider = jointColliders[i].AddComponent<CapsuleCollider>();
-            collider.radius = 1.2f;
-            collider.direction = 2; // direction to z axis
-            collider.height = 10f;
-        }
-    }
-	
+		for (int i = 0; i < numColliders * 2; i++)
+		{
+			string sColObjectName = ((KinectWrapper.NuiSkeletonPositionIndex)(i % numColliders)).ToString() + "Collider";
+			jointColliders[i] = new GameObject(sColObjectName);
+			jointColliders[i].transform.parent = transform;
+
+			CapsuleCollider collider = jointColliders[i].AddComponent<CapsuleCollider>();
+			collider.radius = 0.85f;
+			collider.direction = 2; // direction to z axis
+			collider.height = 3.5f;
+			collider.material = physicMaterial;
+		}
+	}
+
 	// Update is called once per frame
-	void Update () {
-        if (manager == null)
-        {
-            manager = KinectManager.Instance;
-        }
+	void Update()
+	{
+		if (manager == null)
+		{
+			manager = KinectManager.Instance;
+		}
 
-        // get the users texture
-        if (manager && manager.IsInitialized())
-        {
-            //foregroundTex = manager.GetUsersLblTex();
+		// get the users texture
+		if (manager && manager.IsInitialized())
+		{
+			//foregroundTex = manager.GetUsersLblTex();
 
-            if (backgroundImage && (backgroundImage.sprite == null))
-            {
-                //backgroundImage.texture = manager.GetUsersClrTex();
-                backgroundImage.sprite = Sprite.Create(manager.GetUsersClrTex(), new Rect(0, 0, manager.GetUsersClrTex().width, manager.GetUsersClrTex().height), Vector2.zero);
-                backgroundImage.rectTransform.sizeDelta = new Vector2(manager.GetUsersClrTex().width, manager.GetUsersClrTex().height);
-            }
-        }
+			if (backgroundImage && (backgroundImage.sprite == null))
+			{
+				//backgroundImage.texture = manager.GetUsersClrTex();
+				backgroundImage.sprite = Sprite.Create(manager.GetUsersClrTex(), new Rect(0, 0, manager.GetUsersClrTex().width, manager.GetUsersClrTex().height), Vector2.zero);
+				backgroundImage.rectTransform.sizeDelta = new Vector2(manager.GetUsersClrTex().width, manager.GetUsersClrTex().height);
+			}
+		}
 
-        if (manager.IsUserDetected())
-        {
-            uint userId = manager.GetPlayer1ID();
+		if (manager.IsUserDetected())
+		{
+			uint[] userId = new uint[2] { manager.GetPlayer1ID(), manager.GetPlayer2ID() };
 
-            // update colliders
-            int numColliders = (int)KinectWrapper.NuiSkeletonPositionIndex.Count;
+			// update colliders
+			int numColliders = (int)KinectWrapper.NuiSkeletonPositionIndex.Count;
 
-            for (int i = 0; i < numColliders; i++)
-            {
-                if (manager.IsJointTracked(userId, i))
-                {
-                    Vector3 posJoint = manager.GetRawSkeletonJointPos(userId, i);
+			for (int j = 0; j < 2; j++)
+			{
+				for (int i = 0; i < numColliders; i++)
+				{
+					if (manager.IsJointTracked(userId[j], i))
+					{
+						Vector3 posJoint = manager.GetRawSkeletonJointPos(userId[j], i);
 
-                    if (posJoint != Vector3.zero)
-                    {
-                        // convert the joint 3d position to depth 2d coordinates
-                        Vector2 posDepth = manager.GetDepthMapPosForJointPos(posJoint);
+						if (posJoint != Vector3.zero)
+						{
+							// convert the joint 3d position to depth 2d coordinates
+							Vector2 posDepth = manager.GetDepthMapPosForJointPos(posJoint);
 
-                        float scaledX = posDepth.x * foregroundRect.width / KinectWrapper.Constants.DepthImageWidth;
-                        float scaledY = posDepth.y * -foregroundRect.height / KinectWrapper.Constants.DepthImageHeight;
+							float scaledX = posDepth.x * foregroundRect.width / KinectWrapper.Constants.DepthImageWidth;
+							float scaledY = posDepth.y * -foregroundRect.height / KinectWrapper.Constants.DepthImageHeight;
 
-                        float screenX = foregroundOfs.x + scaledX;
-                        float screenY = Camera.main.pixelHeight - (foregroundOfs.y + scaledY);
-                        float zDistance = posJoint.z - Camera.main.transform.position.z;
+							float screenX = foregroundOfs.x + scaledX;
+							float screenY = Camera.main.pixelHeight - (foregroundOfs.y + scaledY);
+							float zDistance = posJoint.z - Camera.main.transform.position.z;
 
-                        Vector3 posScreen = new Vector3(screenX, screenY, zDistance);
-                        Vector3 posCollider = Camera.main.ScreenToWorldPoint(posScreen);
+							Vector3 posScreen = new Vector3(screenX, screenY, zDistance);
+							Vector3 posCollider = Camera.main.ScreenToWorldPoint(posScreen);
 
-                        jointColliders[i].transform.position = posCollider;
-                    }
-                }
-            }
-        }
-    }
+							jointColliders[i + j * numColliders].transform.position = posCollider;
+						}
+					}
+				}
+			}
+		}
+	}
 }
